9a163cc5eabe1cfec45b86adb0335e46c23695ed
Sylvain Lovisi <sylvain.lovisi@gmail.com>
2013-09-05 18:05:21 +0200

    Reorg

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -1,32 +1,105 @@
-module.exports = function(grunt) {
+// Generated on 2013-09-03 using generator-webapp 0.4.1
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
 
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  connect: {
-    server: {
+module.exports = function(grunt) {
+  // configurable paths
+  var yoConf = {
+      dev: 'dev',
+      dist: 'dist'
+  };
+      // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    quickConfig: yoConf,
+    connect: {
       options: {
-        keepalive: true
-      }
-    }
-  },
-  compass: {
-    dist: {
-      options: {
-        config: 'config.rb',
-        environment: 'production'
+        port: 9000,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, quickConfig.dev)
+            ];
+          }
+        }
+      },
+      dist: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, quickConfig.dist)
+            ];
+          }
+        }
       }
     },
-    dev: {
+    open: {
+      server: {
+        path: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+    clean: {
+        dist: {
+            files: [{
+                dot: true,
+                src: [
+                    '.tmp',
+                    '<%= quickConfig.dist %>/*',
+                    '!<%= quickConfig.dist %>/.git*'
+                ]
+            }]
+        },
+        server: '.tmp'
+    },
+    uglify: {
       options: {
-        config: 'config.rb',
-        environment: 'development'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      }
+    },
+    compass: {
+      otions: {
+        config: 'config.rb'
+      },
+      dist: {
+        options: {
+          environment: 'production'
+        }
+      },
+      dev: {
+        options: {
+          environment: 'development'
+        }
       }
     }
-  }
-});
+  });
 
-grunt.loadNpmTasks('grunt-contrib-connect');
-grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.registerTask('build', [
+    'clean',
+    'uglify',
+    'compass',
+    'connect',
+    'open'
+  ]);
 
-grunt.registerTask('default', ['compass','connect']);
+  grunt.registerTask('default', [
+    'clean',
+    'uglify',
+    'compass',
+    'connect',
+    'open'
+  ]);
+
 };
